498914cb7b15888aa58d2fadda4ee6fa2828f431
rferpae569 <rferpae569@g.educaand.es>
2023-06-14 00:29:25 +0200

    ProyectofinGradoRubenFernandez

--- src/app/eleccion2/eleccion2.component.ts
+++ src/app/eleccion2/eleccion2.component.ts
@@ -1,15 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { ServicioService } from '../servicio.service';
+import { Ranking } from '../model/ranking';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+declare var google: any;
 
 @Component({
   selector: 'app-eleccion2',
   templateUrl: './eleccion2.component.html',
   styleUrls: ['./eleccion2.component.scss']
 })
-export class Eleccion2Component {
+export class Eleccion2Component implements OnInit {
 
-  constructor(private router: Router, private cookieService: CookieService) {}
+  datos!: Ranking[];
+  mostrarGrafica: boolean = false;
+  googleChartsLoaded: boolean = false;
+
+  constructor(private router: Router, private cookieService: CookieService, private servicioService: ServicioService) {
+    servicioService.getDatosRanking().subscribe(datos => {
+      this.datos = datos;
+      
+    }); 
+  }
+
+  ngOnInit() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(() => {
+      this.googleChartsLoaded = true;
+    });
+  }
+
+  // guardarRanking() {
+  //   const doc = new jsPDF();
+  
+  //   const columns = ['Nombre', 'Puntos'];
+  //   const data = this.datos.map(dato => [dato.nombre, dato.puntos.toString()]);
+  
+  //   const startY = 20;
+  //   const columnSpacing = 40;
+  //   const rowSpacing = 10;
+  //   const fontSize = 12;
+  
+  //   doc.setFontSize(fontSize);
+  //   doc.text(columns[0], columnSpacing, startY);
+  //   doc.text(columns[1], columnSpacing + 60, startY);
+  
+  //   let currentY = startY + rowSpacing;
+  //   data.forEach(row => {
+  //     doc.text(row[0], columnSpacing, currentY);
+  //     doc.text(row[1], columnSpacing + 60, currentY);
+  //     currentY += rowSpacing;
+  //   });
+  
+  //   doc.save('ranking.pdf');
+  // }
+
+  toggleGrafica() {
+    if (!this.googleChartsLoaded) {
+      return;
+    }
+
+    this.mostrarGrafica = !this.mostrarGrafica;
+
+    if (this.mostrarGrafica) {
+      setTimeout(() => {
+        const data = new google.visualization.DataTable();
+        data.addColumn('number', 'CodigoRanking');
+        data.addColumn('number', 'PuntosImagen');
+        data.addColumn('number', 'PuntosPregunta');
+        data.addColumn('number', 'PuntosMusica');
+
+        // Llenar los datos de la tabla
+        const rows = this.datos.map((dato: any) => [dato.CodigoRanking, dato.PuntosImagen, dato.PuntosPreguntas, dato.PuntosMusica]);
+        data.addRows(rows);
+
+        const options = {
+          title: 'Ranking de Puntos',
+          width: 500,
+          height: 400,
+        };
+
+        const chart = new google.visualization.BarChart(document.getElementById('grafica1'));
+        chart.draw(data, options);
+      }, 0);
+    }
+  }
+
+  // cerrarGrafica() {
+  //   this.mostrarGrafica = false;
+  // }
 
   irAEleccion() {
     // Verificar la existencia de las cookies
@@ -33,5 +114,4 @@ export class Eleccion2Component {
     }
     this.router.navigate(['']);
   }
-
 }
