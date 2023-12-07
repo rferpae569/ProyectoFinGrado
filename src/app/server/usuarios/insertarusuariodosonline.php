6a5fe9dcac1c9d8e6c2d7ad5bfffc14ddab5ddc4
rferpae569 <rferpae569@g.educaand.es>
2023-12-07 19:06:31 +0100

    ProyectofinGradoRubenFernandez

--- src/app/server/usuarios/insertarusuariodosonline.php
+++ src/app/server/usuarios/insertarusuariodosonline.php
@@ -14,40 +14,80 @@ try {
     // Nos conectamos a la base de datos mediante PDO
     $mbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $mbd->beginTransaction();
-    
+
     // Comprobar si NombreUsuario1 y NombreUsuario2 tienen valores
     $stmt = $mbd->prepare("SELECT NombreUsuario1, NombreUsuario2 FROM dosjugadoresonline LIMIT 1");
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if (!empty($row['NombreUsuario1']) && !empty($row['NombreUsuario2'])) {
-        // Si el nombre enviado está en NombreUsuario1, establecer NombreUsuario2 como NULL
-    $sentencia = $mbd->prepare("UPDATE dosjugadoresonline SET NombreUsuario2 = NULL WHERE NombreUsuario1 = :NombreUsuario1");
-    $sentencia->bindParam(':NombreUsuario1', $nombre);
-    $sentencia->execute();
-    
-    $mbd->commit();
-    
-    header('Content-Type: application/json');
-    echo json_encode(array(
-        'msg' => 'NombreUsuario2 establecido como NULL',
-        'NombreUsuario1' => $nombre,
-        'NombreUsuario2' => NULL
-    ));
+
+    if(!empty($row['NombreUsuario1']) && !empty($row['NombreUsuario2'])) {
+        // Si el nombre enviado está en NombreUsuario1, eliminar y actualizar con el nuevo nombre
+        if($row['NombreUsuario1'] === $nombre) {
+            $sentencia = $mbd->prepare("UPDATE dosjugadoresonline SET NombreUsuario1 = :NuevoNombre, NombreUsuario2 = NULL WHERE NombreUsuario1 = :NombreUsuario1");
+            $sentencia->bindParam(':NombreUsuario1', $nombre);
+            $sentencia->bindParam(':NuevoNombre', $nombre);
+            $sentencia->execute();
+
+            $mbd->commit();
+
+            // Espera 10 segundos antes de verificar y eliminar si NombreUsuario2 sigue siendo NULL
+            sleep(10);
+
+            // Verifica si NombreUsuario2 sigue siendo NULL después de 10 segundos
+            $stmt = $mbd->prepare("SELECT NombreUsuario2 FROM dosjugadoresonline WHERE NombreUsuario1 = :NombreUsuario1");
+            $stmt->bindParam(':NombreUsuario1', $nombre);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($row['NombreUsuario2'] === NULL) {
+                // Si NombreUsuario2 sigue siendo NULL, elimina la fila
+                $deleteStmt = $mbd->prepare("DELETE FROM dosjugadoresonline WHERE NombreUsuario1 = :NombreUsuario1");
+                $deleteStmt->bindParam(':NombreUsuario1', $nombre);
+                $deleteStmt->execute();
+
+                header('Content-Type: application/json');
+                echo json_encode(array(
+                    'msg' => 'NombreUsuario1 actualizado con un nuevo nombre y NombreUsuario2 eliminado',
+                    'NombreUsuario1' => $nombre,
+                    'NombreUsuario2' => NULL
+                ));
+            } else {
+                header('Content-Type: application/json');
+                echo json_encode(array(
+                    'msg' => 'NombreUsuario1 actualizado con un nuevo nombre',
+                    'NombreUsuario1' => $nombre,
+                    'NombreUsuario2' => $row['NombreUsuario2']
+                ));
+            }
+        } else {
+            // Si el nombre no coincide con NombreUsuario1, establecer NombreUsuario2 como NULL
+            $sentencia = $mbd->prepare("UPDATE dosjugadoresonline SET NombreUsuario2 = NULL WHERE NombreUsuario1 = :NombreUsuario1");
+            $sentencia->bindParam(':NombreUsuario1', $nombre);
+            $sentencia->execute();
+
+            $mbd->commit();
+
+            header('Content-Type: application/json');
+            echo json_encode(array(
+                'msg' => 'NombreUsuario2 establecido como NULL',
+                'NombreUsuario1' => $row['NombreUsuario1'],
+                'NombreUsuario2' => NULL
+            ));
+        }
     } else {
-        if (!empty($row['NombreUsuario1'])) {
+        if(!empty($row['NombreUsuario1'])) {
             // Si solo NombreUsuario1 tiene valor, actualizar NombreUsuario2
             $sentencia = $mbd->prepare("UPDATE dosjugadoresonline SET NombreUsuario2 = :NombreUsuario2 WHERE NombreUsuario2 IS NULL");
             $sentencia->bindParam(':NombreUsuario2', $nombre);
             $sentencia->execute();
-            
+
             $mbd->commit();
-            
+
             // Obtener ambos nombres después de insertar NombreUsuario2
             $stmt = $mbd->prepare("SELECT NombreUsuario1, NombreUsuario2 FROM dosjugadoresonline LIMIT 1");
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            
+
             header('Content-Type: application/json');
             echo json_encode(array(
                 'msg' => 'Usuario insertado en NombreUsuario2',
@@ -59,9 +99,9 @@ try {
             $sentencia = $mbd->prepare("INSERT INTO dosjugadoresonline (NombreUsuario1) VALUES (:NombreUsuario1)");
             $sentencia->bindParam(':NombreUsuario1', $nombre);
             $sentencia->execute();
-            
+
             $mbd->commit();
-            
+
             header('Content-Type: application/json');
             echo json_encode(array(
                 'msg' => 'Usuario insertado en NombreUsuario1',
