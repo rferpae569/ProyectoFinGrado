d692450d559d2d266bc8d21bcfdf9dc3984043a5
rferpae569 <rferpae569@g.educaand.es>
2024-06-30 13:33:41 +0200

    Info-cookie Rubén Fernández

--- src/app/info-cookie/info-cookie.component.ts
+++ src/app/info-cookie/info-cookie.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-info-cookie',
   templateUrl: './info-cookie.component.html',
-  styleUrls: ['./info-cookie.component.scss']
+  styleUrls: ['./info-cookie.component.scss'],
 })
-export class InfoCookieComponent {
-
-}
+export class InfoCookieComponent {}
