18c3600747adc0a6de8f334351508f018dc40007
rferpae569 <rferpae569@g.educaand.es>
2024-07-04 16:59:19 +0200

    Cartelera Rubén Fernández

--- src/app/cartelera/cartelera.component.ts
+++ src/app/cartelera/cartelera.component.ts
@@ -3,15 +3,16 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-videoclub',
   templateUrl: './cartelera.component.html',
-  styleUrls: ['./cartelera.component.scss']
+  styleUrls: ['./cartelera.component.scss'],
 })
 export class CarteleraComponent implements OnInit {
   percentage: number = 0;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    const moviesContainer = this.elementRef.nativeElement.querySelector('.movies');
+    const moviesContainer =
+      this.elementRef.nativeElement.querySelector('.movies');
     const movies = this.elementRef.nativeElement.querySelectorAll('.movie');
 
     movies.forEach((movie: HTMLElement) => {
@@ -25,7 +26,8 @@ export class CarteleraComponent implements OnInit {
     });
 
     window.addEventListener('scroll', (event: WindowEventMap['scroll']) => {
-      const scrollElement = document.scrollingElement || document.documentElement;
+      const scrollElement =
+        document.scrollingElement || document.documentElement;
       const perc =
         (100 / (scrollElement.scrollHeight - window.innerHeight)) *
         scrollElement.scrollTop;
@@ -42,7 +44,9 @@ export class CarteleraComponent implements OnInit {
       this.renderer.setStyle(
         bg,
         'backgroundPosition',
-        `calc(50% + ${(window.innerWidth / 2) - event.clientX}px * -1) calc(50% + ${(window.innerHeight / 2) - event.clientY}px * -1)`
+        `calc(50% + ${
+          window.innerWidth / 2 - event.clientX
+        }px * -1) calc(50% + ${window.innerHeight / 2 - event.clientY}px * -1)`
       );
     });
   }
